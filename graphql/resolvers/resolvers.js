57f27ef8db6a63c112ee479b265b93668b49e7eb
Maximilian118 <maxcrosby118@gmail.com>
2019-08-12 21:20:48 +0100

    Graphql Auth

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -1,5 +1,42 @@
-const rootResolver = {
+const User = require('../../models/user-schema')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+module.exports = {
+  auth: async args => {
+    try {
+      const userFound = await User.findOne({ email: args.email })
+      if (userFound) {
+        const isPass = await bcrypt.compare(args.password, userFound.password)
+        if (!isPass) {
+          throw new Error("Wrong Password!")
+        }
+        const token = jwt.sign({ userId: userFound._id, email: userFound.email }, process.env.JWT_KEY, { expiresIn: '1h' })
+        console.log(`[auth-resolver] User Logged In...`)
+        return {
+          ...userFound._doc,
+          token: token,
+          tokenExpiry: 1,
+        }
+      }
+      const hashedPass = await bcrypt.hash(args.password, 12)
+      const user = new User({
+        email: args.email,
+        password: hashedPass,
+        img: null
+      })
+      const result = await user.save()
+      console.log(`[auth-resolver] User Created...`)
+      const newUser = await User.findOne({ email: args.email })
+      const token = jwt.sign({ userId: newUser._id, email: newUser.email }, process.env.JWT_KEY, { expiresIn: '1h' })
+      return {
+        ...newUser._doc,
+        password: null,
+        token: token,
+        tokenExpiry: 1
+      }
+    } catch (err) {
+      throw err
+    }
+  },
 }
-
-module.exports = rootResolver
--- graphql/schemas/schema.js
+++ graphql/schemas/schema.js
@@ -2,17 +2,24 @@ const { buildSchema } = require("graphql")
 const { Stream } = require("stream")
 
 module.exports = buildSchema(`
+  type User {
+    _id: ID!
+    email: String!
+    img: String
+    token: String
+    tokenExpiry: Int
+  }
 
-    type rootQuery {
+  type rootQuery {
+    user: User
+  }
 
-    }
+  type rootMutation {
+    auth(email: String!, password: String!): User
+  }
 
-    type rootMutation {
-
-    }
-
-    schema {
-        query: rootQuery
-        mutation: rootMutation
-    }
+  schema {
+    query: rootQuery
+    mutation: rootMutation
+  }
 `)
